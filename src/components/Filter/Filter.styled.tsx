18e6db78ed5964d409a7debd907249b8eb34ded0
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-09-29 19:10:33 +0300

    add_stylked-filter

--- src/components/Filter/Filter.styled.tsx
+++ src/components/Filter/Filter.styled.tsx
@@ -3,11 +3,9 @@ import styled from 'styled-components';
 export const FilterContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 10px 0;
 
   gap: 15px;
-
-  border: 1 solid ${p => p.theme.colors.btn};
 
   @media screen and (min-width: 425px) {
     gap: 20px;
@@ -18,14 +16,153 @@ export const FilterContainer = styled.div`
   }
 `;
 
-export const InputContainer = styled.div`
+export const FilterInputContainer = styled.div`
   display: flex;
   flex-direction: column;
 
-  gap: 10px;
+  gap: 5px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
-export const ButtonContainer = styled.div`
+export const FilterInputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  @media screen and (min-width: 768px) {
+    gap: 10px;
+  }
+`;
+
+export const FilterLabel = styled.label`
+  display: flex;
+  align-items: center;
+  height: 30px;
+
+  @media screen and (min-width: 768px) {
+    height: 40px;
+  }
+`;
+
+export const FilterInput = styled.input`
+  height: 30px;
+  padding: 5px;
+
+  border: 1px solid ${p => p.theme.colors.backgroundSecondary};
+  border-radius: 4px;
+
+  outline: none;
+
+  &:hover,
+  &:focus {
+    border-color: ${p => p.theme.colors.accent};
+  }
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 130px;
+    height: 40px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 230px;
+  }
+`;
+
+export const FilterLabelChecked = styled.label`
+  display: flex;
+  align-items: center;
+
+  height: 30px;
+  gap: 15px;
+
+  cursor: pointer;
+
+  @media screen and (min-width: 768px) {
+    height: auto;
+    flex-direction: column;
+    align-items: center;
+
+    gap: 10px;
+  }
+`;
+
+export const FilterCheckedText = styled.div`
+  display: flex;
+  align-items: center;
+  height: 30px;
+
+  @media screen and (min-width: 768px) {
+    height: 40px;
+  }
+`;
+
+export const FilterInputChecked = styled.input`
+  width: 25px;
+  height: 25px;
+
+  appearance: none;
+
+  border: 1px solid ${p => p.theme.colors.backgroundSecondary};
+  border-radius: 4px;
+
+  cursor: pointer;
+
+  outline: none;
+
+  &:hover,
+  &:focus {
+    border-color: ${p => p.theme.colors.accent};
+  }
+
+  &:checked {
+    background-color: ${p => p.theme.colors.accent};
+    border-color: ${p => p.theme.colors.accent};
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 40px;
+    height: 40px;
+  }
+
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const FilterSelect = styled.select`
+  height: 30px;
+  padding: 5px;
+
+  border: 1px solid ${p => p.theme.colors.backgroundSecondary};
+  border-radius: 4px;
+
+  outline: none;
+
+  &:hover,
+  &:focus {
+    border-color: ${p => p.theme.colors.accent};
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 130px;
+    height: 40px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 230px;
+  }
+`;
+
+export const FilterButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,6 +1,17 @@
 import React, { FC, useEffect, useState } from 'react';
 import { Button } from '../UI/Button/Button.styled';
-import { ButtonContainer, FilterContainer, InputContainer } from './Filter.styled';
+import {
+  FilterButtonContainer,
+  FilterContainer,
+  FilterInputContainer,
+  FilterInputBox,
+  FilterLabel,
+  FilterLabelChecked,
+  FilterCheckedText,
+  FilterInput,
+  FilterInputChecked,
+  FilterSelect,
+} from './Filter.styled';
 import { FilterCars } from '../../types';
 import toast from 'react-hot-toast';
 
@@ -66,20 +77,22 @@ const Filter: FC<FilterProps> = ({ carModel, filterCars, resetFilter }) => {
   return (
     <>
       <FilterContainer>
-        <InputContainer>
-          <label htmlFor="carsInStock">
-            Cars in stock:
-            <input
-              name="inStock"
-              type="checkbox"
-              id="carsInStock"
-              onChange={handleChange}
-              checked={filter.inStock}
-            />
-          </label>
-          <label htmlFor="priceFrom">
-            Price FROM:
-            <input
+        <FilterInputContainer>
+          <FilterInputBox>
+            <FilterLabelChecked htmlFor="carsInStock">
+              <FilterCheckedText>Cars in stock:</FilterCheckedText>
+              <FilterInputChecked
+                name="inStock"
+                type="checkbox"
+                id="carsInStock"
+                onChange={handleChange}
+                checked={filter.inStock}
+              />
+            </FilterLabelChecked>
+          </FilterInputBox>
+          <FilterInputBox>
+            <FilterLabel htmlFor="priceFrom">Price FROM:</FilterLabel>
+            <FilterInput
               name="priceFrom"
               type="number"
               id="priceFrom"
@@ -87,10 +100,10 @@ const Filter: FC<FilterProps> = ({ carModel, filterCars, resetFilter }) => {
               onChange={handleChange}
               value={filter.priceFrom}
             />
-          </label>
-          <label htmlFor="priceTo">
-            Price TO:
-            <input
+          </FilterInputBox>
+          <FilterInputBox>
+            <FilterLabel htmlFor="priceTo">Price TO:</FilterLabel>
+            <FilterInput
               name="priceTo"
               type="number"
               id="priceTo"
@@ -98,35 +111,40 @@ const Filter: FC<FilterProps> = ({ carModel, filterCars, resetFilter }) => {
               onChange={handleChange}
               value={filter.priceTo}
             />
-          </label>
-          <label htmlFor="sortSelect">
-            Sorting by price:
-            <select name="sort" id="sortSelect" onChange={handleChange} value={filter.sort}>
+          </FilterInputBox>
+          <FilterInputBox>
+            <FilterLabel htmlFor="sortSelect">Sorting by price:</FilterLabel>
+            <FilterSelect name="sort" id="sortSelect" onChange={handleChange} value={filter.sort}>
               <option value="No Sorting">No sorting</option>
               <option value="Ascending">Ascending</option>
               <option value="Descending">Descending</option>
-            </select>
-          </label>
-          <label htmlFor="carModelSelect">
-            Car model:
-            <select name="models" id="carModelSelect" onChange={handleChange} value={filter.models}>
+            </FilterSelect>
+          </FilterInputBox>
+          <FilterInputBox>
+            <FilterLabel htmlFor="carModelSelect">Car model:</FilterLabel>
+            <FilterSelect
+              name="models"
+              id="carModelSelect"
+              onChange={handleChange}
+              value={filter.models}
+            >
               <option value="All Models">All Models</option>
               {optin.map((e: string) => (
                 <option key={e} value={e}>
                   {e}
                 </option>
               ))}
-            </select>
-          </label>
-        </InputContainer>
-        <ButtonContainer>
+            </FilterSelect>
+          </FilterInputBox>
+        </FilterInputContainer>
+        <FilterButtonContainer>
           <Button type="button" onClick={handleApply}>
             apply
           </Button>
           <Button type="reset" onClick={handleReset}>
             reset
           </Button>
-        </ButtonContainer>
+        </FilterButtonContainer>
       </FilterContainer>
     </>
   );
